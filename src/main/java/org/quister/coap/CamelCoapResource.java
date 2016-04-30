26e8d978fedd64376b14046918522a466b3c76a6
Joakim Lindquister <joakiml@gmail.com>
2016-04-30 14:33:21 +0200

    Further development

--- src/main/java/org/quister/coap/CamelCoapResource.java
+++ src/main/java/org/quister/coap/CamelCoapResource.java
@@ -4,9 +4,13 @@ import org.apache.camel.Endpoint;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.eclipse.californium.core.CoapResource;
+import org.eclipse.californium.core.coap.CoAP;
+import org.eclipse.californium.core.coap.MediaTypeRegistry;
 import org.eclipse.californium.core.server.resources.CoapExchange;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static org.eclipse.californium.core.coap.CoAP.ResponseCode.INTERNAL_SERVER_ERROR;
 
 class CamelCoapResource extends CoapResource {
     private final Logger logger = LoggerFactory.getLogger(CamelCoapResource.class);
@@ -22,7 +26,7 @@ class CamelCoapResource extends CoapResource {
 
     @Override
     public void handleRequest(org.eclipse.californium.core.network.Exchange exchange) {
-        logger.info("Received Coap request. Converting to camel exchange.");
+        logger.info("Received CoAP request. Converting to camel exchange.");
         CoapExchange coapExchange = new CoapExchange(exchange, this);
         Exchange camelExchange = convertExchange(coapExchange);
 
@@ -32,15 +36,68 @@ class CamelCoapResource extends CoapResource {
             e.printStackTrace();
         }
 
-        String body = camelExchange.getIn().getBody(String.class);
-        coapExchange.respond(body);
+        if (camelExchange.getException() != null) {
+            respondException(coapExchange, camelExchange.getException());
+        } else {
+            respond(coapExchange, camelExchange);
+        }
+    }
+
+    private void respond(CoapExchange coapExchange, Exchange camelExchange) {
+        byte body[];
+
+        if (camelExchange.hasOut()) {
+            body = camelExchange.getOut().getBody(byte[].class);
+        } else {
+            body = camelExchange.getIn().getBody(byte[].class);
+        }
+
+        int httpStatusCode = (int) camelExchange.getIn().getHeader(Exchange.HTTP_RESPONSE_CODE);
+        CoAP.ResponseCode responseCode = getResponseCode(httpStatusCode);
+        logger.info("Responding to CoAP request. CoAP response code: " + responseCode);
+        coapExchange.respond(responseCode, body, MediaTypeRegistry.APPLICATION_OCTET_STREAM);
+    }
+
+    private void respondException(CoapExchange coapExchange, Exception exception) {
+        coapExchange.respond(INTERNAL_SERVER_ERROR, exception.getMessage());
+    }
+
+    private CoAP.ResponseCode getResponseCode(int httpStatusCode) {
+        switch (httpStatusCode) {
+            case 200 :
+                return CoAP.ResponseCode.CONTENT;
+            case 201 :
+                return CoAP.ResponseCode.CREATED;
+            case 204 :
+                return CoAP.ResponseCode.DELETED;
+            case 401 :
+                return CoAP.ResponseCode.UNAUTHORIZED;
+            case 404 :
+                return CoAP.ResponseCode.NOT_FOUND;
+            case 500 :
+                return CoAP.ResponseCode.INTERNAL_SERVER_ERROR;
+            case 504 :
+                return CoAP.ResponseCode.GATEWAY_TIMEOUT;
+            default:
+                if(httpStatusCode >= 200 && httpStatusCode < 300) {
+                    logger.warn("Unknown HTTP code. Setting default success code");
+                    return CoAP.ResponseCode._UNKNOWN_SUCCESS_CODE;
+                } else if (httpStatusCode >= 400 && httpStatusCode < 500) {
+                    logger.warn("Unknown HTTP code. Setting BAD REQUEST");
+                    return CoAP.ResponseCode.BAD_REQUEST;
+                } else {
+                    logger.warn("Unknown HTTP code. Setting internal server error");
+                    return CoAP.ResponseCode.INTERNAL_SERVER_ERROR;
+                }
+
+        }
     }
 
     private Exchange convertExchange(CoapExchange coapExchange) {
         Exchange camelExchange = endpoint.createExchange();
 
-        String body = new String(coapExchange.getRequestPayload());
-        camelExchange.getIn().setBody(body);
+        byte bytes[] = coapExchange.getRequestPayload();
+        camelExchange.getIn().setBody(bytes);
         camelExchange.getIn().setHeader(Exchange.HTTP_METHOD, "GET");
         return camelExchange;
     }
--- src/main/java/org/quister/coap/CoapProducer.java
+++ src/main/java/org/quister/coap/CoapProducer.java
@@ -2,38 +2,79 @@ package org.quister.coap;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.impl.DefaultProducer;
+import org.apache.http.HttpStatus;
 import org.eclipse.californium.core.CoapClient;
 import org.eclipse.californium.core.CoapResponse;
+import org.eclipse.californium.core.coap.CoAP;
+import org.eclipse.californium.core.coap.MediaTypeRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.ConnectException;
+import java.util.Optional;
 
-import static org.eclipse.californium.core.coap.MediaTypeRegistry.TEXT_PLAIN;
 
 class CoapProducer extends DefaultProducer {
-    private static final int HTTP_GATEWAY_TIMEOUT = 504;
     private final Logger logger = LoggerFactory.getLogger(CoapProducer.class);
 
     private final CamelCoapEndpoint endpoint;
+    private final Optional<Long> timeout;
 
-    public CoapProducer(CamelCoapEndpoint endpoint) {
+    public CoapProducer(CamelCoapEndpoint endpoint, long timeout) {
         super(endpoint);
         this.endpoint = endpoint;
+        this.timeout = Optional.of(timeout);
+    }
+
+    public CoapProducer(CamelCoapEndpoint camelCoapEndpoint) {
+        super(camelCoapEndpoint);
+        this.endpoint = camelCoapEndpoint;
+        this.timeout = Optional.empty();
     }
 
     @Override
     public void process(Exchange exchange) throws Exception {
+        logger.info("Producing CoAP request");
         CoapClient client = new CoapClient(endpoint.getEndpointUri());
-        CoapResponse response = client.post(exchange.getIn().getBody().toString(), TEXT_PLAIN);
+        if (timeout.isPresent()) {
+            client.setTimeout(timeout.get());
+        }
+
+        CoapResponse response = client.post(exchange.getIn().getBody(byte[].class), MediaTypeRegistry.APPLICATION_OCTET_STREAM);
 
         if (response == null) {
-            logger.warn("No COAP response received.");
-            exchange.getIn().setHeader(Exchange.HTTP_RESPONSE_CODE, HTTP_GATEWAY_TIMEOUT);
+            client.shutdown();
+            logger.warn("No CoAP response received.");
+            exchange.getIn().setHeader(Exchange.HTTP_RESPONSE_CODE, HttpStatus.SC_GATEWAY_TIMEOUT);
             exchange.getIn().setBody("");
-            throw new ConnectException("No COAP response received");
+            throw new ConnectException("No CoAP response received");
         } else {
-            exchange.getIn().setBody(response.getResponseText());
+            logger.info("CoAP response received. Status code: " + response.getCode());
+
+            exchange.getIn().setBody(response.getPayload());
+            exchange.getIn().setHeader(Exchange.HTTP_RESPONSE_CODE, getHttpStatusCode(response.getCode()));
+        }
+    }
+
+    private int getHttpStatusCode(CoAP.ResponseCode coapResponseCode) {
+        switch(coapResponseCode) {
+            case CONTENT:
+                return HttpStatus.SC_OK;
+            case CREATED:
+                return HttpStatus.SC_CREATED;
+            case DELETED:
+                return HttpStatus.SC_NO_CONTENT;
+            case NOT_FOUND:
+                return HttpStatus.SC_NOT_FOUND;
+            case UNAUTHORIZED:
+                return HttpStatus.SC_UNAUTHORIZED;
+            case INTERNAL_SERVER_ERROR:
+                return HttpStatus.SC_INTERNAL_SERVER_ERROR;
+            case GATEWAY_TIMEOUT:
+                return HttpStatus.SC_GATEWAY_TIMEOUT;
+            default:
+                logger.info("Unknown CoAP response code: " + coapResponseCode + ". Returning 200 OK");
+                return HttpStatus.SC_OK;
         }
     }
 }
